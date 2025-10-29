5913a887904bcdf47ea79d92c1204b14d07a1e8c
Vikthor Vergara <vikthorvergara@gmail.com>
2025-10-28 23:31:36 -0300

    feat: Brigde design pattern example

---
+++ design-patterns/src/main/java/com/github/vikthorvergara/designpatterns/structural/bridge/Remote.java
@@ -0,0 +1,9 @@
+package com.github.vikthorvergara.designpatterns.structural.bridge;
+
+public interface Remote {
+  void power();
+  void volumeDown();
+  void volumeUp();
+  void channelDown();
+  void channelUp();
+}
